9b52808504a0b2990ab7fb7f2051b4d49de5a57a
Gyumin Bae <bgmsound0528@gmail.com>
2026-04-22 22:51:43 +0900

    Add Gradle configuration for Maven Central publishing and CI workflows

--- kooma-core/build.gradle.kts
+++ kooma-core/build.gradle.kts
@@ -1,8 +1,11 @@
-import org.jreleaser.model.Active
+
+import com.vanniktech.maven.publish.SonatypeHost
 import java.time.Year
 
 plugins {
     id("org.jreleaser") version "1.23.0"
+    id("com.vanniktech.maven.publish") version "0.28.0"
+    signing
     `maven-publish`
     `java-library`
 }
@@ -12,70 +15,45 @@ java {
     withSourcesJar()
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = property("project.group").toString()
-            artifactId = property("project.name").toString()
-            version = property("project.version").toString()
+signing {
+    val gpgSecret = project.findProperty("gpg.secret").toString()
+    val gpgPassphrase = project.findProperty("gpg.passphrase").toString()
 
-            pom {
-                name = property("project.name").toString()
-                description = property("project.description").toString()
-                inceptionYear = "${Year.now().value}"
-                url = property("project.url").toString()
-                licenses {
-                    license {
-                        name = property("project.license").toString()
-                        url = property("project.license.url").toString()
-                        distribution = property("project.url").toString()
-                    }
-                }
-                developers {
-                    developer {
-                        id = property("project.developer.id").toString()
-                        name = property("project.developer.name").toString()
-                        email = property("project.developer.email").toString()
-                        url = property("project.developer.url").toString()
-                    }
-                }
-                scm {
-                    url = property("project.url.scm").toString()
-                    connection = "scm:git:git://github.com/${property("project.developer.id")}"
-                    developerConnection = "scm:git:ssh://git@github.com/${property("project.developer.id")}"
-                }
-            }
-            from(components["java"])
-        }
-    }
-    repositories {
-        maven(url = layout.buildDirectory.dir("staging-deploy"))
-    }
+    useInMemoryPgpKeys(gpgSecret, gpgPassphrase)
+    sign(publishing.publications)
 }
 
-jreleaser {
-    gitRootSearch = true
-    deploy {
-        maven {
-            mavenCentral {
-                register("sonatype") {
-                    active = Active.ALWAYS
-                    url = "https://central.sonatype.com/api/v1/publisher?publishing=automatic"
-                    applyMavenCentralRules = true
-                    stagingRepository("build/staging-deploy")
-                }
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, true)
+    coordinates(
+        groupId = property("project.group").toString(),
+        artifactId = property("project.name").toString(),
+        version = property("project.version").toString()
+    )
+    pom {
+        name = property("project.name").toString()
+        description = property("project.description").toString()
+        inceptionYear = "${Year.now().value}"
+        url = property("project.url").toString()
+        licenses {
+            license {
+                name = property("project.license").toString()
+                url = property("project.license.url").toString()
+                distribution = property("project.url").toString()
             }
         }
-    }
-    signing {
-        active = Active.ALWAYS
-    }
-    release {
-        github {
-            repoOwner = property("project.developer.id").toString()
-            repoUrl = property("project.name").toString()
-            releaseName = "v${property("project.version").toString()}"
-            token = System.getenv("JRELEASER_GITHUB_TOKEN")
+        developers {
+            developer {
+                id = property("project.developer.id").toString()
+                name = property("project.developer.name").toString()
+                email = property("project.developer.email").toString()
+                url = property("project.developer.url").toString()
+            }
+        }
+        scm {
+            url = property("project.url.scm").toString()
+            connection = "scm:git:git://github.com/${property("project.developer.id")}"
+            developerConnection = "scm:git:ssh://git@github.com/${property("project.developer.id")}"
         }
     }
 }
