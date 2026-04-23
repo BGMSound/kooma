3439c738c0ccc1f7d0033b9d6bf1abee7fa960bf
Gyumin Bae <bgmsound0528@gmail.com>
2026-04-23 09:47:13 +0900

    Refactor bind function for improved readability in CombinedContextElement

--- kooma-core/src/main/kotlin/io/github/bgmsound/kooma/CombinedContextElement.kt
+++ kooma-core/src/main/kotlin/io/github/bgmsound/kooma/CombinedContextElement.kt
@@ -7,18 +7,19 @@ data class CombinedContextElement(
     override fun flatten() = elements
 
     override fun <T> runWith(block: () -> T): T {
-        fun <T> bind(remaining: List<ContextElement<*>>, block: () -> T): T {
-            if (remaining.isEmpty()) {
-                return block()
-            }
-            val head = remaining.first()
-            @Suppress("UNCHECKED_CAST")
-            return ScopedValue.where(head.scopedValue as ScopedValue<Any?>, head.value)
-                .call<T, Throwable> {
-                    bind(remaining.drop(1), block)
-                }
-        }
 
         return bind(elements, block)
+    }
+
+    private fun <T> bind(remaining: List<ContextElement<*>>, block: () -> T): T {
+        if (remaining.isEmpty()) {
+            return block()
+        }
+        val head = remaining.first()
+        @Suppress("UNCHECKED_CAST")
+        return ScopedValue.where(head.scopedValue as ScopedValue<Any?>, head.value)
+            .call<T, Throwable> {
+                bind(remaining.drop(1), block)
+            }
     }
 }
